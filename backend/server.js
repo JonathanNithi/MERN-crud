5d729da49410cb8d974fc443ba71e18751d022ef
Jonathan Nithiananthan <jonathannithi@gmail.com>
2023-06-02 18:58:08 +0530

    Second commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express()
+
+app.get('/',(req,res) =>{
+    res.send("Hi this is your first message");
+})
+
+app.listen(process.env.port, (req,res) =>{
+    console.log("This is a response");
+})
